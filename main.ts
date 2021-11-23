5522e435ba783960367106aea07e2dea0fadf4e4
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-23 14:46:20 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+pins.digitalWritePin(DigitalPin.P16, 1)
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    }
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+})
