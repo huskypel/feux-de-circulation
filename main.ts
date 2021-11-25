100e47971cea534fe4d52986f66d69183626e0cc
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-25 15:41:45 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,41 @@
-pins.digitalWritePin(DigitalPin.P16, 1)
+function lumiere_verte__pieton () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+input.onButtonPressed(Button.A, function () {
+    if (0 == 0) {
+    	
+    }
+})
+function lumiere_stop_pour_les_pieton () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function lumiere_orange__stop_pieton () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+function lumiere_rouge__non_pieton () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
 basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    }
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-    }
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-    }
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P8, 1)
-    }
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    }
+    lumiere_verte__pieton()
+    basic.pause(2000)
+    lumiere_orange__stop_pieton()
+    basic.pause(2000)
+    lumiere_rouge__non_pieton()
+    basic.pause(2000)
 })
