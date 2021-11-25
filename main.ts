77be5680763053038b779ab65756a0f204b96be9
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-25 16:10:21 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+function lumiere_blanche () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
 function lumiere_verte__pieton () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -6,9 +13,12 @@ function lumiere_verte__pieton () {
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
 input.onButtonPressed(Button.A, function () {
-    if (0 == 0) {
-    	
-    }
+    lumiere_verte__pieton()
+    basic.pause(2000)
+    lumiere_orange__stop_pieton()
+    basic.pause(2000)
+    lumiere_blanche()
+    basic.pause(5000)
 })
 function lumiere_stop_pour_les_pieton () {
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -31,6 +41,10 @@ function lumiere_rouge__non_pieton () {
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
+let pieton = 0
+let lumiere_orange = 0
+let lumiere_rouge = 0
+let lumiere_verte = 0
 basic.forever(function () {
     lumiere_verte__pieton()
     basic.pause(2000)
