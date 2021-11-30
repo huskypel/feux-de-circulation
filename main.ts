edbd57a680b6c9c33544c18b59ed6ab97d2107ce
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-30 09:41:00 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-function lumiere_blanche () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-}
 function lumiere_verte__pieton () {
     pins.digitalWritePin(DigitalPin.P0, 1)
     pins.digitalWritePin(DigitalPin.P1, 0)
@@ -17,8 +10,8 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
     lumiere_orange__stop_pieton()
     basic.pause(2000)
-    lumiere_blanche()
-    basic.pause(5000)
+    lumiere_rouge__non_pieton()
+    basic.pause(2000)
 })
 function lumiere_stop_pour_les_pieton () {
     pins.digitalWritePin(DigitalPin.P0, 0)
@@ -38,8 +31,8 @@ function lumiere_rouge__non_pieton () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
 }
 let pieton = 0
 let lumiere_orange = 0
