c46dc964f72ff9f0bc5b659019f5d3ad95cb7184
huskypel <92863057+huskypel@users.noreply.github.com>
2021-11-30 10:22:27 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,46 +3,34 @@ function lumiere_verte__pieton () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
 }
-input.onButtonPressed(Button.A, function () {
-    lumiere_verte__pieton()
-    basic.pause(2000)
-    lumiere_orange__stop_pieton()
-    basic.pause(2000)
-    lumiere_rouge__non_pieton()
-    basic.pause(2000)
-})
-function lumiere_stop_pour_les_pieton () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function lumiere_orange__stop_pieton () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    pins.digitalWritePin(DigitalPin.P16, 0)
-}
-function lumiere_rouge__non_pieton () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+function lumiereblanche () {
     pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P16, 1)
 }
-let pieton = 0
-let lumiere_orange = 0
-let lumiere_rouge = 0
-let lumiere_verte = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    lumiere = 0
     lumiere_verte__pieton()
     basic.pause(2000)
-    lumiere_orange__stop_pieton()
+    orange()
     basic.pause(2000)
-    lumiere_rouge__non_pieton()
+    lumiereblanche()
     basic.pause(2000)
+    lumiere = 3
+})
+function orange () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+function lumierejaune () {
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+}
+let lumiere = 0
+lumiere_verte__pieton()
+basic.forever(function () {
+    while (lumiere == 3) {
+        lumiere_verte__pieton()
+    }
 })
