9137aa4c949adea27bc9dea139b3f856f5761c2e
huskypel <92863057+huskypel@users.noreply.github.com>
2021-12-01 21:22:01 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,37 @@
-function lumiere_verte__pieton () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-}
-function lumiereblanche () {
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-}
 input.onButtonPressed(Button.A, function () {
-    lumiere = 0
-    lumiere_verte__pieton()
+    vert()
     basic.pause(2000)
     orange()
     basic.pause(2000)
-    lumiereblanche()
+    rouge()
     basic.pause(2000)
-    lumiere = 3
+    lumiere_rouge = 2
 })
 function orange () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P8, 0)
 }
-function lumierejaune () {
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
 }
-let lumiere = 0
-lumiere_verte__pieton()
+function vert () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+}
+let lumiere_rouge = 0
+vert()
 basic.forever(function () {
-    while (lumiere == 3) {
-        lumiere_verte__pieton()
+    if (lumiere_rouge < 1) {
+        vert()
     }
 })
